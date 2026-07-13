1ee8c1bd422a4d89298e2c844aafbfd812f8c8a5
A-amit-dutta <amit8391810917@gmail.com>
2026-07-13 12:03:16 +0530

    Time: 2 ms (16.31%) | Memory: 42.4 MB (98.68%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String sortSentence(String s) {
+        String[] arr = new String[10];
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch !=' '){
+                sb.append(ch);
+            }
+            if(ch == ' ' || i == s.length()-1){
+                int pos = sb.charAt(sb.length()-1)-'0';
+                sb.deleteCharAt(sb.length()-1);
+                arr[pos] = sb.toString();
+            sb.setLength(0);
+            }
+        }
+        sb.setLength(0);
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]!=null ){
+                sb.append(arr[i]+" ");
+            }
+        }
+        return sb.toString().strip();
+    }
+}
