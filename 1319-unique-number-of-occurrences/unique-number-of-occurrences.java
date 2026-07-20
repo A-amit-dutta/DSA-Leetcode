f2635e1ce886df5b456a2e09e6912b5ccde54129
AMIT DUTTA <amit8391810917@gmail.com>
2026-07-20 12:58:41 +0530

    Time: 2 ms (97.95%) | Memory: 43.6 MB (53.00%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -1,14 +1,9 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        Map<Integer, Integer> mpp = new HashMap<>(2005);
+        Map<Integer, Integer> mpp = new HashMap<>();
         for (int ele : arr) {
             mpp.put(ele, mpp.getOrDefault(ele, 0) + 1);
         }
-
-        // int[] hash = new int[2005];
-        // for(int ele : arr){
-        //     hash[ele+1000]++;
-        // }
 
         Set<Integer> st = new HashSet<>();
 
@@ -19,3 +14,19 @@ class Solution {
 
     }
 }
+
+// class Solution {
+//     public boolean uniqueOccurrences(int[] arr) {
+//         Map<Integer, Integer> freq = new HashMap<>();
+//         for (int x : arr) {
+//             freq.put(x, freq.getOrDefault(x, 0) + 1);
+//         }
+
+//         Set<Integer> s = new HashSet<>();
+//         for (int x : freq.values()) {
+//             s.add(x);
+//         }
+
+//         return freq.size() == s.size();
+//     }
+// }
