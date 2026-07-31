e3ae667803eb1c2594517fac101a553cf749fcc7
AMIT DUTTA <amit8391810917@gmail.com>
2026-07-31 17:10:21 +0530

    Time: 6 ms (80.20%) | Memory: 44.2 MB (69.63%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String reverseWords(String s) {
+        s = s.strip();
+        String[] arr = s.split("\\s+");
+        int n = arr.length;
+        StringBuilder sb = new StringBuilder();
+        for(int i=n-1;i>=0;i--){
+            sb.append(arr[i]);
+            if(i!=0)
+                sb.append(" ");
+        }
+        return sb.toString();
+    }
+}
