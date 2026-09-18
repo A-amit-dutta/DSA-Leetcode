26b15677a4167c0780dea230e4e02c385db452af
AMIT DUTTA <amit8391810917@gmail.com>
2026-09-18 22:16:41 +0530

    Time: 2 ms (47.39%) | Memory: 53 MB (27.35%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -11,6 +11,22 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode head1, ListNode head2) {
+        ListNode t1 = head1;
+        ListNode t2 = head2;
+        if(head1 == null || head2 == null) 
+            return null;
+
+        while(t1 != t2)
+        {
+            t1 = t1.next;
+            t2 = t2.next;
+            if(t1 == t2) return t1; 
+            if(t1 == null) t1 = head2;
+            if(t2 == null) t2 = head1;   
+        }        
+        return t1;
+
+    /* 
         Map<ListNode,ListNode> mpp = new HashMap<>();
         ListNode temp1 = head1;
         ListNode temp2 = head2;
@@ -23,9 +39,9 @@ public class Solution {
             if(mpp.containsKey(temp2)){
                 return temp2;
             }
-            // mpp.put(temp1,temp1);
             temp2 = temp2.next;
         }
         return null;
+    */
     }
 }
